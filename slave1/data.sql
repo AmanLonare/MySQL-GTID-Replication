deb8ab34bf09cc28f0995b7882cd6c9a26c0784d
AmanLonare <amanlonare95@gmail.com>
2022-08-08 16:43:48 +0900

    configured ProxySQL

--- slave1/data.sql
+++ slave1/data.sql
@@ -1,2 +1,5 @@
+/* proxysql user */
+CREATE USER IF NOT EXISTS 'monitor'@'%' IDENTIFIED BY 'monitor';
+
 CHANGE MASTER TO MASTER_HOST='mysql_master', MASTER_USER='slave_user', MASTER_PASSWORD='password', MASTER_AUTO_POSITION=1;
 START SLAVE;
