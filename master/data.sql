2ced632ea7993cdbe58f22fae525f35b81be804b
AmanLonare <amanlonare95@gmail.com>
2022-08-18 13:28:23 +0900

    Both master config

--- master/data.sql
+++ master/data.sql
@@ -79,3 +79,7 @@ VALUES (default, '2016-01-16', 1001, 1, 102),
        (default, '2016-01-17', 1002, 2, 105),
        (default, '2016-02-19', 1002, 2, 106),
        (default, '2016-02-21', 1003, 1, 107);
+
+RESET MASTER;
+CHANGE MASTER TO MASTER_HOST='mysql_master2', MASTER_USER='root', MASTER_PASSWORD='root', MASTER_AUTO_POSITION=1;
+START SLAVE;
